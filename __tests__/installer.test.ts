b8b70d01f0f1900012e0fc1d86533f5e90712145
CrazyMax <crazy-max@users.noreply.github.com>
2019-10-12 01:34:53 +0200

    Fix test

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -3,12 +3,12 @@ import * as installer from '../src/installer';
 
 describe('installer', () => {
   it('acquires v0.58.3 version of Hugo', async () => {
-    const hugo = await installer.getHugo('1.8.0', false);
+    const hugo = await installer.getHugo('v0.58.3', false);
     expect(fs.existsSync(hugo)).toBe(true);
   }, 100000);
 
   it('acquires v0.58.3 version of Hugo (extended)', async () => {
-    const hugo = await installer.getHugo('1.8.0', true);
+    const hugo = await installer.getHugo('v0.58.3', true);
     expect(fs.existsSync(hugo)).toBe(true);
   }, 100000);
 
