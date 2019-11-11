45f87b016dc1641ff61ce2fd79c9b2ac0c593ebb
CrazyMax <crazy-max@users.noreply.github.com>
2019-11-12 00:24:46 +0100

    Update prettier

--- src/installer.ts
+++ src/installer.ts
@@ -9,10 +9,7 @@ import * as restm from 'typed-rest-client/RestClient';
 let osPlat: string = os.platform();
 let osArch: string = os.arch();
 
-export async function getHugo(
-  version: string,
-  extended: boolean
-): Promise<string> {
+export async function getHugo(version: string, extended: boolean): Promise<string> {
   const selected = await determineVersion(version);
   if (selected) {
     version = selected;
@@ -21,11 +18,7 @@ export async function getHugo(
   console.log(`✅ Hugo version found: ${version}`);
   const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), 'hugo-'));
   const fileName = getFileName(version, extended);
-  const downloadUrl = util.format(
-    'https://github.com/gohugoio/hugo/releases/download/%s/%s',
-    version,
-    fileName
-  );
+  const downloadUrl = util.format('https://github.com/gohugoio/hugo/releases/download/%s/%s', version, fileName);
 
   console.log(`⬇️ Downloading ${downloadUrl}...`);
   await download.default(downloadUrl, tmpdir, {filename: fileName});
@@ -42,19 +35,11 @@ export async function getHugo(
 }
 
 function getFileName(version: string, extended: boolean): string {
-  const platform: string =
-    osPlat == 'win32' ? 'Windows' : osPlat == 'darwin' ? 'macOS' : 'Linux';
+  const platform: string = osPlat == 'win32' ? 'Windows' : osPlat == 'darwin' ? 'macOS' : 'Linux';
   const arch: string = osArch == 'x64' ? '64bit' : '32bit';
   const ext: string = osPlat == 'win32' ? 'zip' : 'tar.gz';
   const name: string = extended ? 'hugo_extended' : 'hugo';
-  return util.format(
-    '%s_%s_%s-%s.%s',
-    name,
-    version.replace(/^v/, ''),
-    platform,
-    arch,
-    ext
-  );
+  return util.format('%s_%s_%s-%s.%s', name, version.replace(/^v/, ''), platform, arch, ext);
 }
 
 interface GitHubRelease {
@@ -62,24 +47,15 @@ interface GitHubRelease {
 }
 
 async function determineVersion(version: string): Promise<string> {
-  let rest: restm.RestClient = new restm.RestClient(
-    'ghaction-hugo',
-    'https://github.com',
-    undefined,
-    {
-      headers: {
-        Accept: 'application/json'
-      }
+  let rest: restm.RestClient = new restm.RestClient('ghaction-hugo', 'https://github.com', undefined, {
+    headers: {
+      Accept: 'application/json'
     }
-  );
+  });
 
-  let res: restm.IRestResponse<GitHubRelease> = await rest.get<GitHubRelease>(
-    `/gohugoio/hugo/releases/${version}`
-  );
+  let res: restm.IRestResponse<GitHubRelease> = await rest.get<GitHubRelease>(`/gohugoio/hugo/releases/${version}`);
   if (res.statusCode != 200 || res.result === null) {
-    throw new Error(
-      `Cannot find Hugo ${version} release (http ${res.statusCode})`
-    );
+    throw new Error(`Cannot find Hugo ${version} release (http ${res.statusCode})`);
   }
 
   return res.result.tag_name;
