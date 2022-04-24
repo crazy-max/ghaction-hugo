59f3e0f66503a73ec691f56fda8b76c592082078
CrazyMax <crazy-max@users.noreply.github.com>
2022-04-24 16:16:58 +0200

    update dev dependencies and workflow

--- src/github.ts
+++ src/github.ts
@@ -6,7 +6,7 @@ export interface GitHubRelease {
 }
 
 export const getRelease = async (version: string): Promise<GitHubRelease | null> => {
-  const url: string = `https://github.com/gohugoio/hugo/releases/${version}`;
+  const url = `https://github.com/gohugoio/hugo/releases/${version}`;
   const http: httpm.HttpClient = new httpm.HttpClient('ghaction-hugo');
   return (await http.getJson<GitHubRelease>(url)).result;
 };
