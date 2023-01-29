99adb1db13b7c40538f044dc54500453ee8b376e
CrazyMax <crazy-max@users.noreply.github.com>
2023-01-29 04:36:02 +0100

    check latest and tagged releases using releases-json

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -2,6 +2,30 @@ import {describe, expect, it} from '@jest/globals';
 import * as fs from 'fs';
 import * as installer from '../src/installer';
 
+describe('getRelease', () => {
+  it('returns latest hugo GitHub release', async () => {
+    const release = await installer.getRelease('latest');
+    expect(release).not.toBeNull();
+    expect(release?.tag_name).not.toEqual('');
+  });
+
+  it('returns v0.104.2 hugo GitHub release', async () => {
+    const release = await installer.getRelease('v0.104.2');
+    expect(release).not.toBeNull();
+    expect(release?.id).toEqual(78557952);
+    expect(release?.tag_name).toEqual('v0.104.2');
+    expect(release?.html_url).toEqual('https://github.com/gohugoio/hugo/releases/tag/v0.104.2');
+  });
+
+  it('unknown release', async () => {
+    await expect(installer.getRelease('foo')).rejects.toThrowError(
+      new Error(
+        'Cannot find Hugo release foo in https://raw.githubusercontent.com/crazy-max/ghaction-hugo/master/.github/hugo-releases.json'
+      )
+    );
+  });
+});
+
 describe('installer', () => {
   it('acquires v0.104.2 version of Hugo', async () => {
     const hugo = await installer.getHugo('v0.104.2', false);
--- src/installer.ts
+++ src/installer.ts
@@ -1,18 +1,38 @@
 import * as os from 'os';
 import * as path from 'path';
 import * as util from 'util';
-import * as github from './github';
 import * as core from '@actions/core';
+import * as httpm from '@actions/http-client';
 import * as tc from '@actions/tool-cache';
 
 const osPlat: string = os.platform();
 const osArch: string = os.arch();
 
-export async function getHugo(version: string, extended: boolean): Promise<string> {
-  const release: github.GitHubRelease | null = await github.getRelease(version);
-  if (!release) {
-    throw new Error(`Cannot find Mage ${version} release`);
+export interface GitHubRelease {
+  id: number;
+  tag_name: string;
+  html_url: string;
+  assets: Array<string>;
+}
+
+export const getRelease = async (version: string): Promise<GitHubRelease> => {
+  const url = `https://raw.githubusercontent.com/crazy-max/ghaction-hugo/master/.github/hugo-releases.json`;
+  const http: httpm.HttpClient = new httpm.HttpClient('ghaction-hugo');
+  const resp: httpm.HttpClientResponse = await http.get(url);
+  const body = await resp.readBody();
+  const statusCode = resp.message.statusCode || 500;
+  if (statusCode >= 400) {
+    throw new Error(`Failed to get Hugo release ${version} from ${url} with status code ${statusCode}: ${body}`);
   }
+  const releases = <Record<string, GitHubRelease>>JSON.parse(body);
+  if (!releases[version]) {
+    throw new Error(`Cannot find Hugo release ${version} in ${url}`);
+  }
+  return releases[version];
+};
+
+export async function getHugo(version: string, extended: boolean): Promise<string> {
+  const release: GitHubRelease = await getRelease(version);
   const semver: string = release.tag_name.replace(/^v/, '');
 
   core.info(`Hugo version found: ${release.tag_name}`);
